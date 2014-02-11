b53c889608e45bad5c29332a3f0b7eb2c752dd6d
Adrian Medina <adrian.medina@mail.yu.edu>
2014-02-10 19:37:20 -0500

    add ChannelCoercible protocol

--- src/netty/format.clj
+++ src/netty/format.clj
@@ -1,5 +1,13 @@
 (ns netty.format
-  (:import [io.netty.buffer ByteBuf]))
+  (:require [clojure.core.async :as a :refer [put! chan <! >! take!]])
+  (:import [io.netty.buffer ByteBuf]
+           [io.netty.channel ChannelFutureListener]
+           [io.netty.channel.group ChannelGroup DefaultChannelGroup]
+           [io.netty.util.concurrent Future]
+           [java.util.concurrent Executor Executors ThreadFactory]
+           [java.net
+            URLDecoder URLEncoder
+            SocketAddress InetSocketAddress InetAddress]))
 
 (defprotocol AsInputStream
   (as-input-stream [_]))
@@ -10,8 +18,21 @@
 (defprotocol AsChannelBuffer
   (as-channel-buffer [_]))
 
+(defprotocol ChannelCoercible
+  (as-chan [_]))
+
 (extend-protocol AsInputStream)
 
 (extend-protocol AsByteBuffer)
 
 (extend-protocol AsChannelBuffer)
+
+(extend-protocol ChannelCoercible
+  Future
+  (as-chan [future]
+    (let [port (chan 1)]
+      (.addListener future (proxy [ChannelFutureListener] []
+                             (operationComplete [__future]
+                               (when (identical? future __future)
+                                 (put! port __future)))))
+      port)))
